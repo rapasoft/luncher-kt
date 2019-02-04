d47dd580c06f263fc5fe328216750998e7ee8076
Pavol Rajzak <pavol.rajzak@itera.no>
2019-02-04 08:37:36 +0100

    fix test :shrug:

--- src/test/kotlin/eu/rapasoft/service/DailyMenuSourceServiceTest.kt
+++ src/test/kotlin/eu/rapasoft/service/DailyMenuSourceServiceTest.kt
@@ -1,9 +1,27 @@
 package eu.rapasoft.service
 
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext
+import org.slf4j.LoggerFactory
 
 class DailyMenuSourceServiceTest {
+
+    @Before
+    fun before() {
+        StandAloneContext.startKoin(listOf(module {
+            single { LoggerFactory.getLogger("Luncher") }
+            single { DailyMenuSourceService() }
+        }))
+    }
+
+    @After
+    fun after() {
+        StandAloneContext.stopKoin()
+    }
 
     @Test
     fun getSources() {
